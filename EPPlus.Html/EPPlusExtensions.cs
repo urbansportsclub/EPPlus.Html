f5210a3d36df1305ee24dfb3e595e13aad031756
rtm1000 <ron.tamerin@gmail.com>
2018-07-16 23:59:51 +0200

    supports merged cells and embedded pictures
    adding support for merged cells (only within a row) 
    adding support for embedded pictures, converting to base64 inline html source

--- EPPlus.Html/EPPlusExtensions.cs
+++ EPPlus.Html/EPPlusExtensions.cs
@@ -20,6 +20,8 @@ namespace EPPlus.Html
             htmlTable.Attributes["cellspacing"] = 0;
             htmlTable.Styles["white-space"] = "nowrap";
 
+            ProcessPictures(sheet);
+            
             //render rows
             for (int row = 1; row <= lastRow; row++)
             {
@@ -35,8 +37,28 @@ namespace EPPlus.Html
                     HtmlElement htmlCell = htmlRow.AddChild("td");
                     htmlCell.Content = excelCell.Text;
                     htmlCell.Styles.Update(excelCell.ToCss());
+                    
+                    if (excelCell.Merge)
+                    {
+                        int mergeId = sheet.GetMergeCellId(row, col);
+                        //skip next cells until no longer merged on same row
+                        int mergeEndCol = col;
+                        while (mergeEndCol <= lastCol && 
+                               sheet.Cells[row, mergeEndCol].Merge &&
+                               sheet.GetMergeCellId(row,mergeEndCol) == mergeId)
+                        {
+                            mergeEndCol++;
+                        }
+
+                        htmlCell.Attributes["colspan"] = (mergeEndCol - col).ToString();
+                        htmlCell.Styles.Remove("width");
+                        htmlCell.Styles.Remove("max-width");
+                        col = mergeEndCol - 1;
+                    }
                 }
             }
+            
+            RemoveProcessedPictures(sheet);
 
             return htmlTable.ToString();
         }
@@ -44,6 +66,37 @@ namespace EPPlus.Html
         public static CssInlineStyles ToCSS(this ExcelStyles styles)
         {
             throw new NotImplementedException();
+        }
+        
+        private static void ProcessPictures(ExcelWorksheet workSheet)
+        {
+            //update all cells with image content
+            var pictures = workSheet.Drawings.OfType<OfficeOpenXml.Drawing.ExcelPicture>();
+
+            foreach (var pic in pictures)
+            {
+                string imageSource;
+                using (MemoryStream m = new MemoryStream())
+                {
+                    pic.Image.Save(m, System.Drawing.Imaging.ImageFormat.Png);
+
+                    imageSource = string.Format("<img src=\"data:image/png;base64,{0}\"/>", Convert.ToBase64String(m.ToArray()));
+                }
+
+                workSheet.Cells[pic.From.Row + 1, pic.From.Column + 1].Value = imageSource;
+            }
+        }
+
+        private static void RemoveProcessedPictures(ExcelWorksheet workSheet)
+        {
+            //update all cells with image content
+            var pictures = workSheet.Drawings.OfType<OfficeOpenXml.Drawing.ExcelPicture>();
+
+            //remove image data
+            foreach (var pic in pictures)
+            {
+                workSheet.Cells[pic.To.Row + 1, pic.To.Column + 1].Value = null;
+            }
         }
     }
 }
